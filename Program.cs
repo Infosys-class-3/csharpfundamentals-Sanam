4656f24600fb84e3cdf54a949b262430f5a4d34c
Sanam Thapa <thapasanam82@gmail.com>
2022-08-16 20:03:42 +0545

    Added new console

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hey There");
+// Console.Write("What's up!!");
+
+Console.Write("Sanam Thapa");
+var name= Console.ReadLine();
+
+Console.WriteLine("Hello," +name);

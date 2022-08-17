3c9e9460365a08d8e87ca74081875d94fb2afc56
Sanam Thapa <thapasanam82@gmail.com>
2022-08-17 20:20:46 +0545

    Program to check the number is odd ro even

--- Program.cs
+++ Program.cs
@@ -8,9 +8,23 @@
 
 // Console.WriteLine("Hello," +name);
 
-Console.WriteLine("What is your address");
-var address=Console.ReadLine();
-address=address.ToUpper();
-Console.WriteLine(address);
+// Console.WriteLine("What is your address");
+// var address=Console.ReadLine();
+// address=address.ToUpper();
+// Console.WriteLine(address);
+
+Console.WriteLine("Enter the number:");
+var numberText = Console.ReadLine();
+var number=int.Parse(num);
+if (number % 2==0){
+ Console.WriteLine(number + " is even.");}
+else{
+Console.WriteLine(number + "is odd.");}
+ 
+
+
+
+
+
 
 

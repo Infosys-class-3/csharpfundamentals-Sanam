a4c53f6406bd9698efb3862cd2b51957624e0305
Sanam Thapa <thapasanam82@gmail.com>
2022-08-18 06:56:22 +0545

    assigment 2

--- Program.cs
+++ Program.cs
@@ -40,37 +40,28 @@
 //Console.WriteLine(e);
 
 
-//Code to ask name
+
 Console.Write("enter name=");
 string name=Console.ReadLine();
-//code to ask age
 Console.WriteLine("enter age=");
 var age=Console.ReadLine();
 var Age=byte.Parse(age);
-//code to ask weight
 Console.Write("enter weight=");
 short weight=Convert.ToInt16(Console.ReadLine());
-//code to dispaly name age and weight
 Console.WriteLine("name="+name);
 Console.WriteLine("age="+Age);
 Console.WriteLine("weight="+weight);
-
-
-/*
 Console.Write("enter gender=");
 char? gender;
 if(gender=='m'||gender=='M')
-{
-    Console.WriteLine("even number is "+number);
-    Console.Write(gender);
-}
-else
+{Console.WriteLine("even number is "+number);
+    Console.Write(gender);}
 else(gender =='f'||gender='F');
 {
     Console.WriteLine("odd number="+number);
 } 
     Console.Write(gender);
-}*/ 
+
 
 
 

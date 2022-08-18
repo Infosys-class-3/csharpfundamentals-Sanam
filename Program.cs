9fcd65746edbc0634353e1701da037af0c0f3ae6
Sanam Thapa <thapasanam82@gmail.com>
2022-08-18 06:50:20 +0545

    assigment 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//*First Day
 // Console.WriteLine("Hello, World!");
 // Console.WriteLine("Hey There");
 // Console.Write("What's up!!");
@@ -8,18 +8,75 @@
 
 // Console.WriteLine("Hello," +name);
 
-// Console.WriteLine("What is your address");
-// var address=Console.ReadLine();
-// address=address.ToUpper();
-// Console.WriteLine(address);
 
-Console.WriteLine("Enter the number:");
-var numberText = Console.ReadLine();
-var number=int.Parse(num);
-if (number % 2==0){
- Console.WriteLine(number + " is even.");}
-else{
-Console.WriteLine(number + "is odd.");}
+//*Second Day-Task
+ //Console.WriteLine("What is your address");
+ //var address=Console.ReadLine();
+ //address=address.ToUpper();
+ //Console.WriteLine(address);
+
+//*Third Day
+//Console.Write("What is your number?");
+//var numbertext = Console.ReadLine();
+//var number = int.Parse(numbertext);
+
+    //if (number% 2 == 0 )
+    //Console.WriteLine(number + " is even");
+    //else 
+    //Console.WriteLine(number + "is odd");
+
+//*Data-Types
+//int a = 34;
+//float b = 34.4f;
+//double c = 34.4d;
+//bool isgood = true;
+//char d ='r';
+//string e = "This is a string";
+///Console.WriteLine(a);
+//Console.WriteLine(b);
+//Console.WriteLine(c);
+//Console.WriteLine(isgood);
+//Console.WriteLine(d);
+//Console.WriteLine(e);
+
+
+//Code to ask name
+Console.Write("enter name=");
+string name=Console.ReadLine();
+//code to ask age
+Console.WriteLine("enter age=");
+var age=Console.ReadLine();
+var Age=byte.Parse(age);
+//code to ask weight
+Console.Write("enter weight=");
+short weight=Convert.ToInt16(Console.ReadLine());
+//code to dispaly name age and weight
+Console.WriteLine("name="+name);
+Console.WriteLine("age="+Age);
+Console.WriteLine("weight="+weight);
+
+
+/*
+Console.Write("enter gender=");
+char? gender;
+if(gender=='m'||gender=='M')
+{
+    Console.WriteLine("even number is "+number);
+    Console.Write(gender);
+}
+else
+else(gender =='f'||gender='F');
+{
+    Console.WriteLine("odd number="+number);
+} 
+    Console.Write(gender);
+}*/ 
+
+
+
+
+
+
  
 
 

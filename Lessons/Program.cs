88c8c67d26a64f02ae8af233fe39d3aa2c1eb396
Sanam Thapa <thapasanam82@gmail.com>
2022-08-20 21:39:13 +0545

    assigment 3 Create a Employee class..

---
+++ Lessons/Program.cs
@@ -0,0 +1,81 @@
+﻿//*First Day
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hey There");
+// Console.Write("What's up!!");
+
+// Console.Write("Sanam Thapa");
+// var name= Console.ReadLine();
+
+// Console.WriteLine("Hello," +name);
+
+
+//*Second Day-Task
+ //Console.WriteLine("What is your address");
+ //var address=Console.ReadLine();
+ //address=address.ToUpper();
+ //Console.WriteLine(address);
+
+//*Third Day
+//Console.Write("What is your number?");
+//var numbertext = Console.ReadLine();
+//var number = int.Parse(numbertext);
+
+    //if (number% 2 == 0 )
+    //Console.WriteLine(number + " is even");
+    //else 
+    //Console.WriteLine(number + "is odd");
+
+//*Data-Types
+//int a = 34;
+//float b = 34.4f;
+//double c = 34.4d;
+//bool isgood = true;
+//char d ='r';
+//string e = "This is a string";
+///Console.WriteLine(a);
+//Console.WriteLine(b);
+//Console.WriteLine(c);
+//Console.WriteLine(isgood);
+//Console.WriteLine(d);
+//Console.WriteLine(e);
+
+
+
+//Console.Write("enter name=");
+//string name=Console.ReadLine();
+//Console.WriteLine("enter age=");
+//var age=Console.ReadLine();
+//var Age=byte.Parse(age);
+//Console.Write("enter weight=");
+//short weight=Convert.ToInt16(Console.ReadLine());
+//Console.WriteLine("name="+name);
+//Console.WriteLine("age="+Age);
+//Console.WriteLine("weight="+weight);
+//Console.Write("enter gender=");
+///var y = Console.ReadKey;
+//char gender=y.keychar;
+//if(gender=='m'||gender=='M')
+//{Console.WriteLine("even number is "+number);
+  //  Console.Write(gender);}
+//else(gender =='f'||gender='F');
+//{
+    //Console.WriteLine("odd number="+number);
+//} 
+   // Console.Write(gender);
+
+   
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+

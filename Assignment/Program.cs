26cf501346d07136456fe3bb97091ac2f3bc455a
Sanam Thapa <thapasanam82@gmail.com>
2022-08-21 07:09:27 +0545

    Assigment for Employee

---
+++ Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Employee
+{
+    public int EmployeeId;
+    public string? EmployeeName;
+    public long EmployeeContact;
+    public float EmployeeSalary;
+    public string? EmployeeJob;
+    public void Out()
+    {
+        Console.WriteLine(EmployeeId);
+        Console.WriteLine(EmployeeContact);
+        Console.WriteLine(EmployeeJob);
+        Console.WriteLine(EmployeeSalary);
+    }
+    
+
+}
+
+
+class Program1{
+        public static void Main()
+    {
+        Employee employee1 = new Employee();
+        employee1.EmployeeId = 101;
+        employee1.EmployeeName = "Sanam";
+        employee1.EmployeeContact = 9843808000;
+        employee1.EmployeeSalary = 30000;
+        employee1.EmployeeJob = "Developer";
+        employee1.Out();
+
+     
+    }
+}

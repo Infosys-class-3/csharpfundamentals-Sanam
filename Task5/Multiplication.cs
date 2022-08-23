5941f0b43eca2d863ed70ea081359c57a3c91610
Sanam Thapa <thapasanam82@gmail.com>
2022-08-24 05:39:31 +0545

    Assignment Aug 23

---
+++ Task5/Multiplication.cs
@@ -0,0 +1,8 @@
+Console.WriteLine("Table for 5:");
+
+
+for (int t = 5, b = 1; b <= 10; b++)
+{
+    Console.WriteLine(t + "x" + b + "=" + t * b);
+
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Console.WriteLine("Odd numbers Less than 1000:");
+
+// for (int a = 1; a < 1000; a++)
+// {
+//     if (a % 2 != 0 && a != 999)
+//     {
+//         Console.Write(a + ",");
+//     }
+//     if (a == 999)
+//     {
+//         Console.Write(a + "\n");
+
+//     }
+// }
+
+

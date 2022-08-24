89dfa2d69d8b22d045844c62832d27bdb953314e
Sanam Thapa <thapasanam82@gmail.com>
2022-08-24 06:53:20 +0545

    changes

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,16 +1,12 @@
-﻿// Console.WriteLine("Odd numbers Less than 1000:");
+﻿Console.WriteLine("Odd numbers Less than 1000:");
 
-// for (int a = 1; a < 1000; a++)
-// {
-//     if (a % 2 != 0 && a != 999)
-//     {
-//         Console.Write(a + ",");
-//     }
-//     if (a == 999)
-//     {
-//         Console.Write(a + "\n");
-
-//     }
-// }
+for (int a = 1; a < 1000; a++)
+{
+    if (a % 2 != 0 )
+    {
+        Console.Write(a + ",");
+    }
+    
+}
 
 

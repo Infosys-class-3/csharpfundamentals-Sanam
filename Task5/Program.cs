2bb7b27bde16db487642f6fe376789781dae9977
Sanam Thapa <thapasanam82@gmail.com>
2022-08-28 19:38:45 +0545

    Commented unused code

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,14 +1,14 @@
-﻿Console.WriteLine("Odd numbers Less than 1000:");
+﻿// Console.WriteLine("Odd numbers Less than 1000:");
 
-for (int g = 1; g < 1000; g++)
-{
-    if (g % 2 != 0 && g != 999)
-    {
-        Console.Write(g + ",");
-    }
-    if (g == 999)
-    {
-        Console.Write(g + "\n");
+// for (int g = 1; g < 1000; g++)
+// {
+//     if (g % 2 != 0 && g != 999)
+//     {
+//         Console.Write(g + ",");
+//     }
+//     if (g == 999)
+//     {
+//         Console.Write(g + "\n");
 
-    }
-}
+//     }
+// }
